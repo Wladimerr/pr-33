c241852365d9482c15eeb9218cb3ea27bd785be9
Владимир <vladimir@MacBook-Air-Vladimir.local>
2022-02-26 18:33:12 +0700

    Добавлена вторая кнопка, центровка кнопок, изменены цвета и подписи

---
+++ main.py
@@ -0,0 +1,35 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+from kivy.uix.floatlayout import FloatLayout
+
+class myApp(App):
+    def build(self):
+        fl = FloatLayout(size=(200, 200), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        fl.add_widget(Button(
+            text="Кнопка 1",
+            font_size=20,
+            on_press=self.btn1_press,
+            background_color=[1, 0.5, 0, 1],
+            background_normal='',
+            size_hint=(.5, .25),
+            pos_hint={'center_x': 0.5, 'center_y': 0.8}));
+        fl.add_widget(Button(
+            text="Кнопка 2",
+            font_size=20,
+            on_press=self.btn2_press,
+            background_color=[0.5, 0, 1, 1],
+            background_normal='',
+            size_hint=(.5, .25),
+            pos_hint={'center_x': 0.5, 'center_y': 0.5}));
+        return fl
+
+    def btn1_press(self, instance):
+        instance.text = 'Нажата 1'
+
+    def btn2_press(self, instance):
+        instance.text = 'Нажата 2'
+
+
+if __name__ == "__main__":
+    myApp().run()
